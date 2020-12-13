df5071a2b06a1eaef7feab44dc64c9d8b682aec9
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-13 16:19:43 +0200

    auth module

---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wfm-registration',
+  templateUrl: './registration.component.html',
+  styles: [
+  ]
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
