8a9df36eeaf022822d7d66d6c212bfd261394dbf
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-13 17:32:01 +0200

    6. form login : validators

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import {RegistrationComponent} from './registration/registration.component';
 import {AuthComponent} from './auth.component';
 import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from './auth-routing.module';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {AuthRoutingModule} from './auth-routing.module';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     AuthRoutingModule],
 })
 export class AuthModule {
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'wfm-login',
@@ -6,10 +7,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
   }
 
+  submit(): void {
+    console.log(this.form);
+
+  }
 }
