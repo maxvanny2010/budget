9493aa4fba0ae1838915e0da144bd542383cc10a
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-17 22:44:44 +0200

    35 loader

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {LoaderComponent} from './components/loader/loader.component';
 
 @NgModule({
+  declarations: [LoaderComponent],
   imports: [FormsModule, ReactiveFormsModule, HttpClientModule, NgxChartsModule],
-  exports: [FormsModule, ReactiveFormsModule, HttpClientModule, NgxChartsModule],
+  exports: [FormsModule, ReactiveFormsModule, HttpClientModule, NgxChartsModule, LoaderComponent],
 
 })
 export class SharedModule {
