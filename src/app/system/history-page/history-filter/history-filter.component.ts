159f0b0dfc27f2ffd30d628bb50efaceed88fb2a
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2020-12-17 19:55:17 +0200

    modal filters

--- src/app/system/history-page/history-filter/history-filter.component.ts
+++ src/app/system/history-page/history-filter/history-filter.component.ts
@@ -1,16 +1,60 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Category} from '../../shared/models/category';
 
 @Component({
   selector: 'wfm-history-filter',
   templateUrl: './history-filter.component.html',
   styleUrls: ['./history-filter.component.scss']
 })
-export class HistoryFilterComponent implements OnInit {
+export class HistoryFilterComponent {
+  @Output() filterCancel = new EventEmitter<any>();
+  @Output() filterApply = new EventEmitter<any>();
 
-  constructor() {
+  @Input() categories: Category[] = [];
+  selectedTypes = [];
+  selectedPeriod = 'd';
+  selectedCategories = [];
+  timePeriods = [
+    {type: 'd', label: 'День'},
+    {type: 'w', label: 'Неделя'},
+    {type: 'M', label: 'Месяц'}
+  ];
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'},
+  ];
+
+  closeFilter(): void {
+    this.selectedTypes = [];
+    this.selectedPeriod = 'd';
+    this.selectedCategories = [];
+    this.filterCancel.emit();
   }
 
-  ngOnInit(): void {
+  private calculateInputParams(field: string, event: Event): void {
+    const value = event.target[`value`];
+    const checked = event.target[`checked`];
+    if (checked) {
+      // tslint:disable-next-line:no-unused-expression
+      !this[`${field}`].includes(value) ? this[`${field}`].push(value) : null;
+    } else {
+      this[`${field}`] = this[`${field}`].filter(i => i !== value);
+    }
   }
 
+  changeType(event: Event): void {
+    this.calculateInputParams('selectedTypes', event);
+  }
+
+  changeCategory(event: Event): void {
+    this.calculateInputParams('selectedCategories', event);
+  }
+
+  applyFilter(): void {
+    this.filterApply.emit({
+      types: this.selectedTypes,
+      categories: this.selectedCategories,
+      period: this.selectedPeriod,
+    });
+  }
 }
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -4,6 +4,7 @@ import {map} from 'rxjs/operators';
 import {Category} from '../shared/models/category';
 import {WFMEvent} from '../shared/models/event.model';
 import {CategoriesService} from '../shared/services/categories.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'wfm-history-page',
@@ -13,10 +14,13 @@ import {CategoriesService} from '../shared/services/categories.service';
 export class HistoryPageComponent implements OnInit, OnDestroy {
   categories: Category[] = [];
   events: WFMEvent[] = [];
+  filterEvents: WFMEvent[] = [];
   chartData = [];
   isLoaded = false;
 
   cSub: Subscription;
+
+  isFilterView = false;
 
   constructor(private categoryService: CategoriesService,
               private eventService: CategoriesService) {
@@ -33,15 +37,20 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
       .subscribe((data) => {
         this.categories = data[0];
         this.events = data[1];
+        this.setOriginalEvents();
         this.calculateChartData();
         this.isLoaded = true;
       });
   }
 
+  private setOriginalEvents(): void {
+    this.filterEvents = this.events.slice();
+  }
+
   private calculateChartData(): void {
     this.chartData = [];
     this.categories.forEach((cat) => {
-      const catEvent = this.events.filter(e => e.category === cat.id && e.type === 'outcome');
+      const catEvent = this.filterEvents.filter(e => e.category === cat.id && e.type === 'outcome');
       this.chartData.push({
         name: cat.name,
         value: catEvent.reduce((total, e) => {
@@ -56,5 +65,44 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
     if (this.cSub) {
       this.cSub.unsubscribe();
     }
+  }
+
+  private toggleViewFilter(dir: boolean): void {
+    this.isFilterView = dir;
+  }
+
+  openFilter(): void {
+    this.toggleViewFilter(true);
+  }
+
+  filterApply(filterData: any): void {
+    this.setOriginalEvents();
+    this.toggleViewFilter(false);
+
+    const startPeriod = moment().startOf(filterData.period).startOf('d');
+    const endPeriod = moment().endOf(filterData.period).endOf('d');
+    this.filterEvents = this.filterEvents
+      .filter((e) => {
+        return filterData.types.indexOf(e.type) !== -1;
+      })
+      .filter((e) => {
+        return filterData.categories.indexOf(e.category.toString()) !== -1;
+      })
+      .filter((e) => {
+        const momentDate = moment(e.date, 'DD.MM.YYYY HH:mm:ss');
+        return momentDate.isBetween(startPeriod, endPeriod);
+      });
+    this.calculateChartData();
+  }
+
+  filterCancel(): void {
+    this.toggleViewFilter(false);
+    this.setOriginalEvents();
+    this.calculateChartData();
+  }
+
+  resetFilters(): void {
+    this.setOriginalEvents();
+    this.calculateChartData();
   }
 }
